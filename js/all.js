1d0eb95bc08431a512d86800c637226eb4d29c0c
amanoizumi <a0918233246@gmail.com>
2022-03-11 05:31:16 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,46 +1,38 @@
-
-window.onload = function() {
+window.onload = function () {
   const grid = document.querySelector('.grid');
-
   const box = document.querySelectorAll('.box');
   const imgArr = document.querySelectorAll('.box img');
   const len = imgArr.length;
   let imgShow = null;
-  
-  const msnry = new Masonry( grid, {
+
+  const msnry = new Masonry(grid, {
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
-    percentPosition: true
+    percentPosition: true,
   });
-  
-  
-  
-  imagesLoaded( grid ).on( 'progress', () => {
+
+  imagesLoaded(grid).on('progress', () => {
     msnry.layout();
     imgAnimation();
   });
-  
 
-  
   let i = 0;
   const imgAnimation = () => {
     if (i < len && i > 0) {
-  
-      box[i-1].classList.remove('hidden');
-      box[i-1].classList.add('bounceIn');
+      box[i - 1].classList.remove('hidden');
+      box[i - 1].classList.add('bounceIn');
     }
-  }
+  };
 
   const count = () => {
     imgShow = setInterval(() => {
-      i++
+      i++;
       imgAnimation();
       if (i === 11) {
         clearInterval(imgShow);
       }
     }, 300);
-  }
+  };
 
   count();
-}
-
+};
